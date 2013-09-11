433f7a7a5feeba59d3be374dc52d3ac051a0e428
Elias Martenson <lokedhs@gmail.com>
2013-09-12 00:45:17 +0800

    Fix the cache check

--- template.lisp
+++ template.lisp
@@ -10,6 +10,7 @@
    (modified-date   :type integer
                     :reader parsed-file-modified-date)
    (last-time-check :type integer
+                    :initarg :last-time-check
                     :accessor parsed-file-last-time-check)
    (template        :type function
                     :reader parsed-file-template
@@ -20,6 +21,10 @@
 (defmethod initialize-instance :after ((obj parsed-file) &key &allow-other-keys)
   (setf (slot-value obj 'modified-date) (file-write-date (parsed-file-pathspec obj)))
   (setf (slot-value obj 'last-time-check) (get-universal-time)))
+
+(defvar *template-cache-min-check* 1
+  "The minimum number of seconds between checks whether the template
+file have been changed")
 
 (defvar *cached-templates* (make-hash-table :test 'equal))
 (defvar *cached-templates-lock* (bordeaux-threads:make-lock "cached-templates-lock"))
@@ -35,18 +40,22 @@ output to STREAM."
   (bordeaux-threads:with-lock-held (*cached-templates-lock*)
     (let* ((pathname (pathname file))
            (cached (gethash pathname *cached-templates*)))
-      (funcall (parsed-file-template (cond ((or (null cached)
-                                                (> (file-write-date (parsed-file-pathspec cached))
-                                                   (parsed-file-modified-date cached)))
-                                            (setf (gethash pathname *cached-templates*)
-                                                  (make-instance 'parsed-file
-                                                                 :name pathname
-                                                                 :template (parse-template-file pathname
-                                                                                                :binary binary
-                                                                                                :encoding encoding))))
-                                           (t
-                                            (setf (parsed-file-last-time-check cached) (get-universal-time))
-                                            cached)))
+      (funcall (parsed-file-template (if (or (null cached)
+                                             (and (> (- (get-universal-time)
+                                                        (parsed-file-last-time-check cached))
+                                                     *template-cache-min-check*)
+                                                  (> (file-write-date (parsed-file-pathspec cached))
+                                                     (parsed-file-modified-date cached))))
+                                         ;; The file needs to be reparsed
+                                         (setf (gethash pathname *cached-templates*)
+                                               (make-instance 'parsed-file
+                                                              :name pathname
+                                                              :template (parse-template-file pathname)
+                                                              :binary binary
+                                                              :encoding encoding
+                                                              :last-time-check (get-universal-time)))
+                                         ;; The cached file is up to date                                         
+                                         cached))
                data stream))))
 
 (defun exec-template-to-string (file data)
